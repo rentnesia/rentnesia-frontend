4fd953a67eae0c8589f12ed0dfe3d8f33891994c
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2019-01-29 09:36:24 +0700

    change dir images and make header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
-import React from "react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import PropTypes from "prop-types";
+import $ from "jquery";
 
-class Header extends React.Component {
+import { Link } from "react-router-dom";
+
+class Header extends Component {
   static propTypes = {
     state: PropTypes.object
   };
@@ -13,16 +17,70 @@ class Header extends React.Component {
     children: null
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {};
+  componentDidMount() {
+    $(window).scroll(() => {
+      const navbar = $("#main-navbar");
+      const logoNav = $("#logo-absolute");
+      if ($(window).scrollTop() >= 1) {
+        navbar.removeClass("navbar-absolute").addClass("navbar-fixed");
+        logoNav.removeClass("logo").addClass("logo-white");
+      } else {
+        navbar.removeClass("navbar-fixed").addClass("navbar-absolute");
+        logoNav.removeClass("logo-white").addClass("logo");
+      }
+    });
   }
 
   render() {
     return (
-      <div>
-        <h1>ini header</h1>
-      </div>
+      <nav className="navbar navbar-expand-lg navbar-absolute" id="main-navbar">
+        <div className="container">
+          <Link to="#">
+            <div id="logo-navbar" className="logo" />
+          </Link>
+          <div className="collapse navbar-collapse" id="menu-navbar">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to="#" className="nav-link">
+                  CATEGORIES <i className="fas fa-chevron-down" />
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="#" className="nav-link">
+                  <h6 className="fas fa-search" aria-hidden="true" />
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  <h6 className="fas fa-shopping-cart" aria-hidden="true" />
+                </a>
+              </li>
+              <li className="nav-item mr-0">
+                <Link
+                  to="#"
+                  className="btn btn-sm btn-animate btn-animate-side-right btn-transparent-danger secondary btn-login"
+                >
+                  <span>
+                    LOGIN
+                    <i className="icon fas fa-arrow-right" aria-hidden="true" />
+                  </span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="#"
+                  className="btn btn-sm btn-animate btn-animate-side-right btn-danger btn-join"
+                >
+                  <span>
+                    SIGNUP
+                    <i className="icon fas fa-sign-in-alt" aria-hidden="true" />
+                  </span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
+import OwlCarousel from "react-owl-carousel2";
+import "react-owl-carousel2/src/owl.carousel.css";
 
 import Shell from "../components/Shell";
+
+const options = {
+  items: 1,
+  navText: [
+    `<img src='/images/carousel/button/chevron-left.png'>`,
+    `<img src='/images/carousel/button/chevron-right.png'>`
+  ],
+  nav: true,
+  rewind: true,
+  autoplay: true
+};
 
 class Home extends Component {
   constructor(props) {
@@ -10,7 +23,29 @@ class Home extends Component {
   render() {
     return (
       <Shell>
-        <h1>Hello</h1>
+        <OwlCarousel id="home-carousel" options={options}>
+          <div>
+            <img
+              className="w-100"
+              src="/images/carousel/calleven_web.png"
+              alt="Call Even"
+            />
+          </div>
+          <div>
+            <img
+              className="w-100"
+              src="/images/carousel/freeboxchange.png"
+              alt="Free Box Change"
+            />
+          </div>
+          <div>
+            <img
+              className="w-100"
+              src="/images/carousel/newyouweb.png"
+              alt="Ikea"
+            />
+          </div>
+        </OwlCarousel>
       </Shell>
     );
   }
