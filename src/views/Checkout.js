5d2c480d0fec996584402d9552ff3492753f9310
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-02-07 19:46:49 +0700

    fix state in checkout + add paymentform

--- src/views/Checkout.js
+++ src/views/Checkout.js
@@ -6,56 +6,103 @@ import Shell from "../components/Shell";
 import OffCanvas from "react-aria-offcanvas";
 
 const AddressForm = () => (
-  <div className="location col-6 flex-fix overlay-maroon">
-    <div className="top">
-      <h2>New Address</h2>
+  <div className="location">
+    <div className="col-12 overlay-maroon">
+      <div className="top">
+        <h2>New Address</h2>
+      </div>
+      <div className="bottom">
+        <form className="form">
+          <input
+            className="form-control"
+            type="text"
+            name="address"
+            placeholder="Address"
+            style={{ margin: "15px 0" }}
+          />
+          <input
+            className="form-control"
+            type="text"
+            name="address-optional"
+            placeholder="Apartment, suite, etc. (optional)"
+            style={{ margin: "15px 0" }}
+          />
+          <input
+            className="form-control"
+            type="text"
+            name="city"
+            placeholder="City"
+            style={{ margin: "15px 0" }}
+          />
+          <input
+            className="form-control"
+            type="text"
+            name="state"
+            placeholder="State"
+            style={{ margin: "15px 0" }}
+          />
+          <input
+            className="form-control"
+            type="number"
+            name="zip"
+            placeholder="Zip"
+            style={{ margin: "15px 0" }}
+          />
+          <div className="button-form">
+            <button className="btn btn-sm btn-success btn-submit">
+              <span>Add address</span>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-    <div className="bottom">
-      <form className="form">
-        <input
-          className="form-control"
-          type="text"
-          name="address"
-          placeholder="Address"
-          style={{ margin: "15px 0" }}
-        />
-        <input
-          className="form-control"
-          type="text"
-          name="address-optional"
-          placeholder="Apartment, suite, etc. (optional)"
-          style={{ margin: "15px 0" }}
-        />
-        <input
-          className="form-control"
-          type="text"
-          name="city"
-          placeholder="City"
-          style={{ margin: "15px 0" }}
-        />
-        <input
-          className="form-control"
-          type="text"
-          name="state"
-          placeholder="State"
-          style={{ margin: "15px 0" }}
-        />
-        <input
-          className="form-control"
-          type="number"
-          name="zip"
-          placeholder="Zip"
-          style={{ margin: "15px 0" }}
-        />
-        <div className="pull-right">
-          <button className="btn btn-sm btn-animate btn-animate-vertical btn-success btn-submit">
-            <span>
-              <i className="icon fa fa-paper-plane" aria-hidden="true" />
-              Add address
-            </span>
-          </button>
+  </div>
+);
+
+const PaymentForm = () => (
+  <div className="payment">
+    <div className="col-12 overlay-maroon">
+      <div className="top">
+        <h2>Payment information</h2>
+      </div>
+      <div className="bottom">
+        <div>
+          <form className="form">
+            <input
+              className="form-control"
+              type="text"
+              name="card-name"
+              placeholder="Cardholder name"
+              style={{ margin: "15px 0" }}
+            />
+            <input
+              className="form-control"
+              type="number"
+              name="card-number"
+              placeholder="Card Number"
+              style={{ margin: "15px 0" }}
+            />
+            <div className="button-form">
+              <button className="btn btn-sm btn-success btn-submit">
+                <span>Save</span>
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
+    </div>
+  </div>
+);
+
+const PickupForm = () => (
+  <div className="pickup">
+    <div className="col-12 overlay-maroon">
+      <div className="top">
+        <h2>Choose a Pickup Time</h2>
+      </div>
+      <div className="bottom">
+        <form className="form" />
+      </div>
     </div>
   </div>
 );
@@ -63,20 +110,84 @@ const AddressForm = () => (
 class Checkout extends Component {
   constructor(props) {
     super(props);
+    this.onLocationClick = this.onLocationClick.bind(this);
+    this.onPickupClick = this.onPickupClick.bind(this);
+    this.onPaymentClick = this.onPaymentClick.bind(this);
     this.state = {
-      isOpen: false
+      isOpenNewItem: false,
+      isOpenLocation: false,
+      isOpenPickup: false,
+      isOpenPayment: false
     };
   }
 
-  open = () => {
+  open = e => {
+    e.preventDefault();
+    const currentState = this.state.isOpenNewItem;
+    console.log(currentState);
+
     this.setState({
-      isOpen: true
+      isOpenNewItem: !currentState,
+      isOpenLocation: false,
+      pickupStatus: false,
+      paymentStatus: false
     });
   };
 
-  close = () => {
+  closeNewItem = () => {
     this.setState({
-      isOpen: false
+      isOpenNewItem: false
+    });
+  };
+
+  closeLocation = () => {
+    this.setState({
+      isOpenLocation: false
+    });
+  };
+
+  closePickup = () => {
+    this.setState({
+      isOpenPickup: false
+    });
+  };
+
+  closePayment = () => {
+    this.setState({
+      isOpenPayment: false
+    });
+  };
+
+  onLocationClick = e => {
+    e.preventDefault();
+    const currentState = this.state.isOpenLocation;
+    console.log(currentState);
+
+    this.setState({
+      isOpenLocation: !currentState,
+      isOpenNewItem: false,
+      pickupStatus: false,
+      paymentStatus: false
+    });
+  };
+
+  onPickupClick = e => {
+    e.preventDefault();
+    const currentState = this.state.isOpenPickup;
+    this.setState({
+      isOpenPickup: !currentState,
+      locationStatus: false,
+      paymentStatus: false
+    });
+  };
+
+  onPaymentClick = e => {
+    e.preventDefault();
+    const currentState = this.state.isOpenPayment;
+    this.setState({
+      isOpenPayment: !currentState,
+      locationStatus: false,
+      pickupStatus: false
     });
   };
 
@@ -97,64 +208,95 @@ class Checkout extends Component {
                   color="secondary"
                   size="sm"
                   block
-                  aria-expanded={this.state.isOpen}
+                  aria-expanded={this.state.isOpenNewItem}
                   onClick={this.open}
                 >
                   New Items
                 </Button>
-              </div>
-              <div className="checkout-details-section">
-                <h5>Address</h5>
-                <Button
-                  class="btn"
-                  outline
-                  color="secondary"
-                  size="sm"
-                  block
-                  aria-expanded={this.state.isOpen}
-                  onClick={this.open}
-                >
-                  Add a location
-                </Button>
                 <OffCanvas
-                  isOpen={this.state.isOpen}
-                  onClose={this.close}
+                  isOpen={this.state.isOpenNewItem}
+                  onClose={this.closeNewItem}
                   labelledby="menu-button"
                 >
-                  <div className="btn-close" onClick={this.close}>
+                  <div className="btn-close" onClick={this.closeNewItem}>
                     <i className="fa fa-times" />
                   </div>
-                  <AddressForm />>
+                  <h1>hello</h1>
                 </OffCanvas>
               </div>
+
               <div className="checkout-details-section">
-                <h5>Pick-up</h5>
                 <Button
-                  class="btn btn-light"
+                  className="btn btn-light"
                   outline
                   color="secondary"
                   size="sm"
                   block
-                  aria-expanded={this.state.isOpen}
-                  onClick={this.open}
+                  aria-expanded={this.state.isOpenLocation}
+                  onClick={this.onLocationClick}
+                >
+                  Add a Location
+                </Button>
+                <OffCanvas
+                  isOpen={this.state.isOpenLocation}
+                  onClose={this.closeLocation}
+                  labelledby="menu-button"
+                >
+                  <div className="btn-close" onClick={this.closeLocation}>
+                    <i className="fa fa-times" />
+                  </div>
+                  <AddressForm />
+                </OffCanvas>
+              </div>
+
+              <div className="checkout-details-section">
+                <Button
+                  className="btn btn-light"
+                  outline
+                  color="secondary"
+                  size="sm"
+                  block
+                  aria-expanded={this.state.isOpenPickup}
+                  onClick={this.onPickupClick}
                 >
                   Pickup time
                 </Button>
+                <OffCanvas
+                  isOpen={this.state.isOpenPickup}
+                  onClose={this.closePickup}
+                  labelledby="menu-button"
+                >
+                  <div className="btn-close" onClick={this.closePickup}>
+                    <i className="fa fa-times" />
+                  </div>
+                  <h1>Pickup</h1>
+                </OffCanvas>
               </div>
+
               <div className="checkout-details-section">
-                <h5>Payment</h5>
                 <Button
-                  class="btn btn-light"
+                  className="btn btn-light"
                   outline
                   color="secondary"
                   size="sm"
                   block
-                  aria-expanded={this.state.isOpen}
-                  onClick={this.open}
+                  aria-expanded={this.state.isOpenPayment}
+                  onClick={this.onPaymentClick}
                 >
                   Please enter payment information
                 </Button>
+                <OffCanvas
+                  isOpen={this.state.isOpenPayment}
+                  onClose={this.closePayment}
+                  labelledby="menu-button"
+                >
+                  <div className="btn-close" onClick={this.closePickup}>
+                    <i className="fa fa-times" />
+                  </div>
+                  <PaymentForm />
+                </OffCanvas>
               </div>
+
               <div className="checkout-details-section">
                 <h5>Notes for your Concierge</h5>
                 <Input
